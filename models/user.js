b8a1a81aa6839d4ee8280a9a5ff77cc742f81d6e
Saran Pariyar <spariyar05@gmail.com>
2024-02-13 21:18:41 +0545

    Created user schema

---
+++ models/user.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: String,
+    password: String,
+    email:String
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//checks if model named Topic already exists or not in mongoose application
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
