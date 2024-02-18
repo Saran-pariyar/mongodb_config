c3ca667e6fad3f81e5601d04af66ed76aa875d32
Saran Pariyar <spariyar05@gmail.com>
2024-02-18 18:55:11 +0545

    created signout btn

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 // import SignInBtn from "./SignInBtn";
 import SignInBtn from "./SignInBtn"
 
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 
 export default function UserInfo() {
   const { status, data: session } = useSession();
@@ -13,18 +13,24 @@ export default function UserInfo() {
     return (
       <div className="shadow-xl p-8 rounded-md flex flex-col gap-3 bg-yellow-200">
         {/* we have to add lh3.googleusercontent.com in next.config.js to make the image work  */}
-        <Image
+        {/* <Image
           className="rounded-full"
           src={session?.user?.image}
           width={60}
           height={60}
-        />
+        /> */}
         <div>
           Name: <span className="font-bold">{session?.user?.name}</span>
         </div>
         <div>
           Email: <span className="font-bold">{session?.user?.email}</span>
         </div>
+        <button
+          onClick={() => signOut()}
+          className="bg-slate-900 text-white px-6 py-2 rounded-md"
+        >
+          Sign Out
+        </button>
       </div>
     );
   } else {
