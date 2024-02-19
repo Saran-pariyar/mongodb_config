67103b98da734f42a03606a15c9d538b243d39ec
Saran Pariyar <spariyar05@gmail.com>
2024-02-19 22:05:16 +0545

    Added user to database after sign in

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+// import clientPromise from "../../../../lib/mongodb"
+import connectMongoDB from "@/lib/mongodb";
+import User from "@/models/user";
 
 const authOptions ={
 providers: [
@@ -7,7 +10,45 @@ providers: [
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-  ]
+  ],
+  callbacks:{
+async signIn({user, account}){
+  //this will show in terminal
+  // console.log("User " , user);
+  // console.log("Account ", account);
+
+if(account.provider === "google"){
+  const { name, email } = user;
+        try {
+          await connectMongoDB();
+          const userExists = await User.findOne({ email });
+
+          if (!userExists) {
+            const res = await fetch("http://localhost:3000/api/users", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                name,
+                email,
+              }),
+            });
+
+            if (res.ok) {
+              console.log("User added to the database!");
+              return user;
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+}
+
+
+  return user;
+}
+  }
 }
 
 
--- models/user.js
+++ models/user.js
@@ -3,7 +3,6 @@ import mongoose, { Schema } from "mongoose";
 const userSchema = new Schema(
   {
     name: String,
-    password: String,
     email:String
   },
   {
